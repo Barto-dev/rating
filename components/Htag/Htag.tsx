2727539c4097785522151cc9cad2fff2fc9211ef
Barto <illya1004work@gmail.com>
2021-06-28 09:36:50 +0300

    add title component

---
+++ components/Htag/Htag.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {HtagProps} from "./Htag.props";
+
+const Htag = ({tag, children} : HtagProps): JSX.Element => {
+  return (
+    <h1>{children}</h1>
+  );
+};
+
+export default Htag;
