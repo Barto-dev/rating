396e79791474388e17ec4c5af9efde1184356646
Barto <illya1004work@gmail.com>
2021-06-28 15:43:05 +0300

    create Tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Htag from "../components/Htag/Htag";
 import Button from "../components/Button/Button";
 import P from '../components/P/P';
+import Tag from "../components/Tag/Tag";
 
 export default function Home(): JSX.Element {
 
@@ -10,6 +11,7 @@ export default function Home(): JSX.Element {
       <Button appearance='primary' arrow='right'>Кпопка</Button>
       <Button appearance='ghost' arrow='down'>Кпопка</Button>
       <P size={'s'}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, incidunt.</P>
+      <Tag color={'red'} href={'google.com'}>111</Tag>
     </>
   );
 }
