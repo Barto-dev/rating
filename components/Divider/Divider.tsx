52052072d43fca663329b3a1bcf26392857ede8c
Barto <illya1004work@gmail.com>
2021-07-10 09:53:15 +0300

    product component

---
+++ components/Divider/Divider.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Divider.module.css';
+
+import cn from 'classnames';
+import {DividerProps} from "./Divider.props";
+
+const Divider = ({className, ...props}:DividerProps):JSX.Element => {
+  return (
+  <hr className={cn(className, styles.hr)} {...props}/>
+  );
+};
+
+export default Divider;
