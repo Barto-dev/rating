f4c4b3e3993f89f1c80dd1ee32958859a9dac18f
Barto <illya1004work@gmail.com>
2021-07-12 20:42:58 +0300

    forward ref

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, {ForwardedRef, forwardRef} from 'react';
 import cn from 'classnames';
 import styles from './Card.module.css';
 import {CardProps} from "./Card.props";
 
-const Card = ({children, color = 'white', className, ...props}: CardProps): JSX.Element => {
+const Card = forwardRef(({children, color = 'white', className, ...props}: CardProps, ref: ForwardedRef<HTMLDivElement>): JSX.Element => {
   return (
     <div className={cn(styles.card, className, {
       [styles.blue] : color === 'blue'
-    })} {...props}>
+    })} {...props} ref={ref}>
       {children}
     </div>
   );
-};
+});
 
 export default Card;
