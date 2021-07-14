f13e9ba89b52064209443b54e306bf1969c65794
Barto <illya1004work@gmail.com>
2021-07-14 22:06:33 +0300

    animation

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {ButtonProps} from "./Button.props";
 import styles from './Button.module.css';
 import cn from "classnames";
+import {motion, useMotionValue} from 'framer-motion';
 
 import ArrowIcon from './arrow.svg';
 
 const Button = ({children, appearance, arrow = 'none', className, ...props}: ButtonProps): JSX.Element => {
+  const scale = useMotionValue(1);
+
+/*
+  useEffect(() => {
+    scale.onChange( s => console.log(s));
+  }, []);
+*/
+
 
   return (
-    <button className={cn(styles.button, className, {
+    <motion.button whileHover={{scale: 1.05}} className={cn(styles.button, className, {
       [styles.primary]: appearance === 'primary',
       [styles.ghost]: appearance === 'ghost'
     })}
-            {...props}
+                   style={{scale}}
+                   {...props}
     >
       {children}
       {arrow !== 'none' && <span className={cn(styles.arrow, {
         [styles.down]: arrow === 'down'
       })}><ArrowIcon /></span>}
-    </button>
+    </motion.button>
   );
 };
 
